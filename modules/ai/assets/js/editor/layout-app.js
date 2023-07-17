6c32cc1d4d38e4b0a61188fa3193568784c7c43d
Nevo Golan <20494189+Nevoss@users.noreply.github.com>
2023-07-17 17:42:14 +0300

    wip

--- modules/ai/assets/js/editor/layout-app.js
+++ modules/ai/assets/js/editor/layout-app.js
@@ -1,6 +1,6 @@
 import { ThemeProvider, DirectionProvider } from '@elementor/ui';
 
-const LayoutApp = ( { isRTL, colorScheme, onClose } ) => {
+const LayoutApp = ( { isRTL, colorScheme, onClose, onResolve } ) => {
 	return (
 		<DirectionProvider rtl={ isRTL }>
 			<ThemeProvider colorScheme={ colorScheme }>
@@ -14,6 +14,7 @@ LayoutApp.propTypes = {
 	colorScheme: PropTypes.oneOf( [ 'auto', 'light', 'dark' ] ),
 	isRTL: PropTypes.bool,
 	onClose: PropTypes.func,
+	onResolve: PropTypes.func,
 };
 
 export default LayoutApp;
